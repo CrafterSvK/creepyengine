11a658ad1de5427c1e1e978bd92fd06fe9552af2
Jakub Janek <shaman.janek@gmail.com>
2017-04-14 11:43:47 +0200

    Added debug code

--- src/World.py
+++ src/World.py
@@ -1,37 +1,14 @@
 from random import randint
-from xml.etree import ElementTree
+import XML
 
 
 class World:
+    xml = XML.XML()
+
     def generate_tile(self, x, y):
-
         type_land = randint(1, 3)
-        number_entities = randint(1, 20)
-        active_entities = randint(1, number_entities)
-        passive_entities = number_entities - active_entities
-        land = [x, y, type_land, number_entities, passive_entities, active_entities]
+        type_land_s = self.xml.get_text_by_id(type_land, "world", "name")
+        list_type_xml = self.xml.get_list_by_id(type_land, "world", "type")
+        land = "You are in " + type_land_s + ". It is ", list_type_xml, " in here!"
 
-        if type_land == 1:
-            type_land_s = "desert"
-            active_entities_s = "cojots"
-            passive_entities_s = "cactuses"
-        elif type_land == 2:
-            type_land_s = "moutains"
-            active_entities_s = "birds"
-            passive_entities_s = "rocks"
-        elif type_land == 3:
-            type_land_s = "plain"
-            active_entities_s = "wolfs"
-            passive_entities_s = "trees"
-        else:
-            type_land_s = "void xD"
-
-        print("On", x, "x", y, "there's", type_land_s, "and", active_entities, active_entities_s, passive_entities, passive_entities_s, "!")
         return land
-
-    def get_file(self):
-        tree = ElementTree.parse('xml/entities.xml')
-        root = tree.getroot()
-        for child in root:
-            print(child.tag, child.attrib)
-        return 0
